9d06893d8fbcec1c533d16c2392c2253d686964b
raycode21 <wanjiku.rachel21@gmail.com>
2017-11-14 07:26:14 +0300

    roomtest initial commit

--- Tests/testRoom.py
+++ Tests/testRoom.py
@@ -1,18 +1,24 @@
 import unittest
-from room import Room
+from unittest import TestCase
+from src.room import Room, Office, LivingSpace
 
-class RoomClassTest(unittest.testcase):
+class RoomClassTest(Testcase):
     def test_room_instance(self):
         blue = Room('blue')
         self.assertIsInstance(blue, Room, msg = 'The object should be a type of Room')
+        self.current_occupancy = len(blue.self.memberList)
+
 
     def test_create_room(self):
+        blue = Room('blue')
         blue_office = blue.create_room('Blue', 'office')
         self.assertTrue(blue_office)
-        current_occupancy = len(blue.memberList)
 
     def test_check_occupancy(self):
-        self.assertEqual((current_occupancy < max_occupancy), 'current_occupancy should be less the maximum')
+        self.assertGreater (self.current_occupancy, self.max_occupancy, 'current_occupancy should be less the maximum')
+class LivingSpaceClassTest(Testcase):
+        def test_allocate_by_gender(self):
+            assertEqual({self.room_gender: 'female',self.Person.gender : 'F'}, msg = 'please fill in gender')
 
         if __name__ == '__main__':
             unittest.main()
---
+++ room.py
@@ -0,0 +1,52 @@
+import Person
+class Room(object):
+
+    def __init__(self, room_name, room_type):
+        self.room_name = room_name
+        self.room_type = room_type
+        self.member_list = []
+        self.max_occupancy = 0
+        self.current_occupancy = len(self.member_list)
+
+    def create_room(self):
+        print (self.room_name, self.room_type)
+        print('Room created successfully!')
+
+    def add_member(self, new_member):
+        self.member_list.append(new_member)
+
+    def get_members(self):
+        self.member_list
+
+    def check_occupancy(self):
+        if self.current_occupancy == self.max_occupancy:
+            print ('This room is full to capacity..sorry')
+        else:
+            print ('This room still has available space!')
+
+
+class Living_space(Room):
+    livingspace_by_gender = {'male':[], 'female':[]}
+
+    def __init__(self, room_name, room_gender):
+        super(Living_space, self).__init__(room_name,room_gender)
+        self.room_name = room_name
+        self.max_occupancy = 4
+        self.room_gender = room_gender
+        self.wants_livingspace = False
+
+    def allocation_by_gender(self):
+        while self.current_occupancy < self.max_occupancy and self.wants_livingspace is True:
+            if self.room_gender == 'female' and Person.gender == 'F':
+                self.livingspace_by_gender['female'].append(Person)
+            else:
+                 self.livingspace_by_gender['male'].append(Person)
+                 break
+
+
+
+class Office(Room):
+    def __init__(self, room_name):
+        super(Office, self).__init__(room_name)
+        self.room_name = room_name
+        self.max_occupancy = 6
