5d1d40d1757505e385b1043b256d11600d1169e1
raycode21 <wanjiku.rachel21@gmail.com>
2017-11-15 06:06:13 +0300

    changes in tests

--- src/dojo.py
+++ src/dojo.py
@@ -1,16 +1,4 @@
-from abc import ABCmeta, abstractMethod
-__metaclass__ = ABCmeta
+from src.room import *
+from src.person import *
 
-@abstractMethod
-def something(self):
-        pass
-        
 class Dojo(object):
-    
-    def __init__(self, name, occupancy):
-        self.name = name
-        self.occupancy = 0
-        
-    
-        
-
